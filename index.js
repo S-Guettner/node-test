c916b591ef3cff4c6aee2a9bf9303fb6bfaf3541
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-20 20:53:50 +0100

    fixed bug on win with opening IE

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+//import http modul
 import * as http from 'http'
 
+//creating server
 const server = http.createServer((req, res) => {
     res.write("Hello World!")
     res.end()
 })
-
+//set server port
 server.listen(3_000)
